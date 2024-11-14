5ebd9b8cc8e84e9e5e20f9c42a2d1fbc78f30389
Martin <martin.evenbom@gmail.com>
2024-11-14 19:19:00 +0100

    bubbletea view, init, update. setup complete

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base64"
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 	"os"
-   _ "strings" 
-    "time"
+	"strings"
+	_ "strings"
+	"time"
 
-	// tea "github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
 	_ "github.com/charmbracelet/wish"
-
 	// bm "github.com/charmbracelet/wish/bubbletea"
 )
 
@@ -103,5 +103,108 @@ func logAccess(fingerprint, status string) {
         log.Printf("Error writing to log: %v", err)
     }
 }
+
+func (m Model) Init() tea.Cmd {
+    return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c", "q":
+            if m.currentPage != "main" {
+                m.currentPage = "main"
+                m.showLink = false
+                return m, nil
+            }
+            return m, tea.Quit
+
+        case "up", "k":
+            if m.cursor > 0 {
+                m.cursor--
+            }
+
+        case "down", "j":
+            if m.currentPage == "projects" {
+                if m.cursor < len(m.projects)-1 {
+                    m.cursor++
+            }
+        } else {
+            if m.cursor < len(m.choices)-1 {
+                m.cursor++
+            }
+        }
+
+    case "enter", " ":
+        if m.currentPage == "main" {
+            m.currentPage = strings.ToLower(strings.Replace(m.choices[m.cursor], " ", "", -1))
+            m.cursor = 0
+        } else if m.currentPage == "projects" {
+            m.showLink = !m.showLink
+        }
+    }
+}
+    return m, nil
+}
+
+func (m Model) View() string {
+    var s strings.Builder
+
+    s.WriteString(headerStyle.Render("Connected with key fingerprint: \n"))
+    s.WriteString(fmt.Sprintf("%s\n\n", m.userFingerprint))
+
+    switch m.currentPage {
+    case "main":
+        s.WriteString(headerStyle.Render("Welcome to Martin's Portfolio!\n\n"))
+        s.WriteString("Use ↑/↓ arrows to navigate, or j/k, enter to select, q to quit\n\n")
+
+        for i, choice := range m.choices {
+            cursor := " "
+            if m.cursor == i {
+                cursor = ">"
+            }
+            s.WriteString(fmt.Sprintf("%s %s\n", cursor, choice))
+        }
+    case "projects":
+        s.WriteString(headerStyle.Render("=== Projects ===\n\n"))
+        s.WriteString("Use↑/↓ to navigate, enter to show/hide links, q to go back\n\n")
+
+        for i, project := range m.projects {
+            cursor := " "
+            if m.cursor == i {
+                cursor = ">"
+            }
+            s.WriteString(fmt.Sprintf("%s %s\n", cursor, project.name))
+            s.WriteString(fmt.Sprintf("   Description: %s\n", project.description))
+            s.WriteString(fmt.Sprintf("   Tech Stack: %s\n", project.techStack))
+            if m.showLink && m.cursor == i {
+                s.WriteString(fmt.Sprintf("   Link: %s\n", linkStyle.Render(project.link)))
+            }
+            s.WriteString("\n")
+        }
+    
+    case "aboutme":
+        s.WriteString(headerStyle.Render("=== About Me ===\n"))
+        s.WriteString("Software Developer etc.")
+        s.WriteString("\nPress q to go back")
+
+    case "skills":
+        s.WriteString(headerStyle.Render("=== Skills ===\n"))
+        s.WriteString("Languages: Python, Golang, Javascript, SQL etc\n")
+        s.WriteString("Frameworks: Flask, Django\n")
+        s.WriteString("Tools: Vim, Docker, Linux, Azure\n")
+        s.WriteString("\nPress q to go back")
+
+    case "contact":
+        s.WriteString(headerStyle.Render("=== Contact ===\n"))
+        s.WriteString("Email: martin.evenbom@gmail.com\n")
+        s.WriteString("Github: github.com/zibiax\n")
+        s.WriteString("\nPress q to go back")
+    }
+
+    return s.String()
+}
+
 
 
